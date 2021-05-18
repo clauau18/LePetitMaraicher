1d12837388f69afe99a1b89542ee6b67684e51ed
Claux Aurélien <aurelclaux@gmail.com>
2021-05-18 20:40:18 +0200

    ajouter des legumes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -15,6 +16,7 @@ import { PaymentPageComponent } from './payment-page/payment-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -35,7 +37,9 @@ import {MatButtonModule} from '@angular/material/button';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    HttpClientModule,
+    FormsModule
 
   ],
   providers: [],
--- src/app/buying-page/buying-page.component.ts
+++ src/app/buying-page/buying-page.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./buying-page.component.scss']
 })
 export class BuyingPageComponent implements OnInit {
+  nameVegetable: string | undefined;
+  priceVegetable: string | undefined;
+  quantityVegetable: string = "1 kg";
+
+  vegetables:Array<{nameVegetable: string, priceVegetable: string, quantityVegetable: string}> =  new Array<{nameVegetable: string, priceVegetable: string, quantityVegetable: string}>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  addVegetable(): void {
+    var vegetable: any = {
+      id:Math.random(),
+      nameVegetable: this.nameVegetable,
+      priceVegetable: this.priceVegetable,
+      quantityVegetable: this.quantityVegetable
+    }
+    this.nameVegetable = ""; // sera bindé dans la page HTML
+    this.priceVegetable = "";
+    this.vegetables.push(vegetable);
+  }
+
+  deleteNode(vegetable:any): void {
+    var index = this.vegetables.indexOf(vegetable);
+    this.vegetables.splice(index,1);
   }
 
 }
