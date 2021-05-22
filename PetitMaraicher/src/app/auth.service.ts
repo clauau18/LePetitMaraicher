62929f48f576b07506f4dd669ff95cccfef4442f
Zeina Al Wazzan <zeina@macbook-air-de-zeina.home>
2021-05-22 17:22:55 +0200

    auth.service

--- PetitMaraicher/src/app/auth.service.ts
+++ PetitMaraicher/src/app/auth.service.ts
@@ -1,9 +1,40 @@
-import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AuthService {
+    connectedUser:any = null;
+    constructor(private http: HttpClient) {
+        this.isLogged();
+    }
 
-  constructor() { }
+    login(username:any, password:any):Observable<any>{
+        return this.http.post("http://localhost:3000/login", {username:username, password:password}, {withCredentials: true}); 
+    }
+
+    logout():Observable<any>{
+        console.log("ddd")
+        return this.http.get("http://localhost:3000/login", {withCredentials: true});
+    }
+
+    register(username:any, password:any, fullName:any):Observable<any>{
+        return this.http.post("http://localhost:3000/register", {username: username, password:password, fullName:fullName},{withCredentials: true});
+    }
+
+    isLogged(){
+        this.http.post("http://localhost:3000/islogged", {withCredentials : true}).subscribe (
+            (connectedUser)=> {
+                this.connectedUser = connectedUser;
+                console.log(this.connectedUser)
+                console.log("connected")
+            },
+            (error) => {
+                console.log("not connected")
+            }
+        )
+    }
+
 }
