3ad29cb3cd5daa6d76fb08cc7518198a7f847286
Zeina Al Wazzan <zeina@macbook-air-de-zeina.home>
2021-05-22 11:10:54 +0200

    css cnxn pages

--- src/app/connexion-page/connexion-page.component.ts
+++ src/app/connexion-page/connexion-page.component.ts
@@ -1,24 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
+import {FormControl, Validators} from '@angular/forms';
 
 
 @Component({
-  selector: 'app-connexion-page',
+  selector: 'app-connexion-page, form-field-error-example',
   templateUrl: './connexion-page.component.html',
   styleUrls: ['./connexion-page.component.scss']
 })
 export class ConnexionPageComponent implements OnInit {
 
-  username:any = "";
+  login:any = "";
   password:any = "";
 
   constructor(public authService: AuthService) { }
 
   ngOnInit(): void {
   }
-
+  
   submit():any{
-    this.authService.login(this.username, this.password).subscribe(
+    this.authService.login(this.login, this.password).subscribe(
       (userInfo:any)=>{
         this.authService.connectedUser = userInfo;
       },
